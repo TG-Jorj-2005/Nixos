a96378572bd197900d44be969d7b122008246654
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-15 22:20:11 +0300

    Automated commit luni 15 septembrie 2025, 22:20:11 +0300

--- assets/nvf-configuration.nix
+++ assets/nvf-configuration.nix
@@ -1,11 +1,10 @@
 { pkgs, lib, ... }:
 {
-vim.theme.enable = true;
-vim.theme.name = "gruvbox";
-vim.theme.style = "dark";
-
-vim.languages.nix.enable = true;
-
+vim={
+  theme={
+       name = "gruvbox";
+       style = "dark";
+};
 statusline.lualine.enable = true;
 telescope.enable = true;
 autocomplete.nvim-cmp.enable = true;
@@ -18,6 +17,6 @@ languages = {
   ts.enable = true;
   rust.enable = true;
  
-
+   };
  };
 }
