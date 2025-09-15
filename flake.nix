db04285968de00cc3e50419da2d5a6d6fb747935
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-15 22:08:28 +0300

    Automated commit luni 15 septembrie 2025, 22:08:28 +0300

--- flake.nix
+++ flake.nix
@@ -7,23 +7,30 @@
        url = "github:nix-community/home-manager/master";
        inputs.nixpkgs.follows = "nixpkgs";
        };
+       nvf.url = "github:notashelf/nvf";
        pyprland.url = "github:hyprland-community/pyprland";
 
  };
 
-  outputs = { self, nixpkgs, home-manager, pyprland,  ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, pyprland, nvf,  ... }@inputs:
   let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
 in
    {
+   
+    packages.system.default = (nvf.lib.neovimConfiguration{
+    inherit pkgs;
+    modules = [ ./assets/nvf-configuration.nix];
+      }).neovim;
 
     nixosConfigurations={
     Nixos-JRJ-BRW = lib.nixosSystem {
       inherit system;
       modules = [
          ./configuration.nix
+         nvf.nixosModules.default
       ];
     };
   };
