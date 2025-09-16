8d1e56df854a7b240ab8d4379a33a6edbf6efc65
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-16 14:05:26 +0300

    Automated commit marți 16 septembrie 2025, 14:05:26 +0300

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -16,7 +16,7 @@ programs.neovim = {
     nvim-lspconfig
     neodev-nvim
     telescope-nvim
-    nerdtree
+    nvim-tree-lua
     nvim-treesitter
     nvim-web-devicons
     catppuccin-nvim
@@ -159,6 +159,42 @@ programs.neovim = {
         }
       },
     }
+
+    safe_setup("nvim-tree", {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = true,
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {}
+  },
+  view = {
+    width = 30,
+    side = 'left',
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+        folder = true,
+        file = true,
+      }
+    }
+  }
+})
     
     -- Aplică setup() pentru fiecare plugin din listă
     for plugin_name, config in pairs(plugin_configs) do
