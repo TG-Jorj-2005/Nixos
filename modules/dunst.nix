985bd4e28ad38b7cfb4ddb03cb3a5dec333f1224
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-16 15:18:19 +0300

    Automated commit marți 16 septembrie 2025, 15:18:19 +0300

--- modules/dunst.nix
+++ modules/dunst.nix
@@ -4,36 +4,45 @@
   services.dunst = {
     enable = true;
     package = pkgs.dunst;
-    settings = {
-      font = "JetBrains Mono Medium 10";
-      frame_width = 2;
-      separator_height = 0;
-      transparency = 0;
-      format = "%s";
-      time_format = "%H:%M";
-      ignore_newline = true;
-      geometry = "300x50-10+10";
-      sort = "urgency";
-      startup_notification = false;
-      mouse = true;
-      max_icon_size = 32;
-      icon_position = "left";
-      line_height = 1.2;
-      word_wrap = true;
-      show_age_threshold = 5;
-      frame_color = "#458588";
-      low_background = "#3c3836";
-      low_foreground = "#ebdbb2";
-      normal_background = "#282828";
-      normal_foreground = "#ebdbb2";
-      critical_background = "#cc241d";
-      critical_foreground = "#fbf1c7";
-      urgency_low = { timeout = 5; };
-      urgency_normal = { timeout = 10; };
-      urgency_critical = { timeout = 0; };
-                                     };
-    
+    iconTheme = "Papirus-Dark";
+    configFile = ''
+      [global]
+      follow = mouse
+      width = 400
+      height = 300
+      font = JetBrains Mono 10
+      origin top-right
+      offset= 0x25
+      padding = 10
+      separator_height = 2
+      frame_width = 2
+      frame_color = "#4c566a"
+      separator_color = frame 
+      format = "<b>%s</b>\n%b"
+      alignment = left
+      vertical_alignment = center
+      min_icon_size = 0
+      max_icon_size = 32
+      corner_radius = 4
 
-    };
+      [urgency_low]
+      background = "#2e3440"
+      foreground = "#d8dee9"
+      timeout = 5
+      frame_color = "#4c566a"
+      
+      [urgency_normal]
+      background = "#3b4252"
+      foreground = "#eceff4"
+      timeout = 10
+      frame_color = "#81a1c1"
+      new_icon = bell
+       
+      [urgency_critical]  
+      background = "#bf616a"
+      foreground = "#2e3440"
+      timeout = 0
+      frame_color = "#d08770"
+    '';
 
 }
