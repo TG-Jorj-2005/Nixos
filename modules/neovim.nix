45b5f2420e86ff14e0e460630f3535a1fcad0da6
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-16 13:49:49 +0300

    Automated commit marți 16 septembrie 2025, 13:49:49 +0300

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -77,6 +77,72 @@ programs.neovim = {
     }
 
     vim.cmd.colorscheme("catppuccin")
+
+        -- Configurare nvim-cmp separată
+    local ok_cmp, cmp = pcall(require, 'cmp')
+    local ok_luasnip, luasnip = pcall(require, 'luasnip')
+    
+    if ok_cmp and ok_luasnip then
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+      
+      -- Configurare pentru search
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      
+      -- Configurare pentru command line
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+      
+      print("✓ Loaded: nvim-cmp")
+    else
+      print("✗ Failed to load: nvim-cmp or luasnip")
+    end
+    
+    -- Lista plugin-urilor cu configurări
+    local plugin_configs = {
+      neodev = {},
+      telescope = {},
+      ["nvim-treesitter.configs"] = {
+        ensure_installed = {},
+        highlight = { enable = true },
+        indent = { enable = true },
+      },
+      lualine = {
+        options = {
+          theme = "catppuccin"
+        }
+      },
+    }
     
     -- Aplică setup() pentru fiecare plugin din listă
     for plugin_name, config in pairs(plugin_configs) do
