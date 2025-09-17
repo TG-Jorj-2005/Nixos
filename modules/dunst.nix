3637e92320a8094a653ce1ad283f1e8194e2affd
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-17 11:40:14 +0300

    Automated commit miercuri 17 septembrie 2025, 11:40:14 +0300

--- modules/dunst.nix
+++ modules/dunst.nix
@@ -12,43 +12,32 @@
   };
 
   home.file."~/.config/dunst/dunstrc".text = ''
-    [global]
-    follow = mouse
-    width = 400
-    height = 300
-    font = JetBrains Mono 10
-    origin = top-right
-    offset= 0x25
-    padding = 10
-    separator_height = 2
-    frame_width = 2
-    frame_color = "#4c566a"
-    separator_color = frame 
-    format = "<b>%s</b>\n%b"
-    alignment = left
-    vertical_alignment = center
-    min_icon_size = 0
-    max_icon_size = 32
-    corner_radius = 4
+   [global]
+frame_color = "#cad3f5"
+separator_color= frame
+font = "JetBrains Mono Regular 11"
+corner_radius = 10
+offset = 5x5
+origin = top-right
+notification_limit = 8
+gap_size = 7
+frame_width = 2
+width = 300
+height = 100
+follow = keyboard
 
-    [urgency_low]
-    background = "#2e3440"
-    foreground = "#d8dee9"
-    timeout = 5
-    frame_color = "#4c566a"
-    
-    [urgency_normal]
-    background = "#3b4252"
-    foreground = "#eceff4"
-    timeout = 10
-    frame_color = "#81a1c1"
-    new_icon = bell
-     
-    [urgency_critical]  
-    background = "#bf616a"
-    foreground = "#2e3440"
-    timeout = 0
-    frame_color = "#d08770"
-  '';
+[urgency_low]
+background = "#24273A"
+foreground = "#CAD3F5"
+
+[urgency_normal]
+background = "#24273A"
+foreground = "#CAD3F5"
+
+[urgency_critical]
+background = "#24273A"
+foreground = "#CAD3F5"
+    frame_color = "#F5A97F"
+    '';
 }
 
